b3216f866b6f581b6b43c1a7789a58da7cc20648
WU Shengzhi <wushengzhi891215@gmail.com>
2021-09-12 00:32:48 +0800

    Update faq.tsx
    update some text in FAQ

--- packages/thedate-frontend/pages/faq.tsx
+++ packages/thedate-frontend/pages/faq.tsx
@@ -34,15 +34,15 @@ export default function FAQPage() {
           </p>
           <p>
             The Date is metadata. It could be a building block of future Metaverse ecosystem. 
-            You can use it in anyway you wants, such as storytelling, gifting, memorization, building an artwork, or use it in metaverses. 
+            You can use it in whatever ways, such as storytelling, gifting, memorization, building an artwork, or use it in Metaverse. 
           </p>
         </div>
 
         <div className="faq__item">
           <h3>How can I own The Date of today? </h3>
           <p>
-          The Date of today will be minted as a non-fungible token through an auction 
-          opening from 00:00:00 UTC to 23:59:59 UTC each day. 
+          The Date of today will be minted as a non-fungible token through an auction. 
+          Opening from 00:00:00 UTC to 23:59:59 UTC each day. 
           The highest bidder will own The Date of that day. 
           </p>
 
@@ -54,7 +54,7 @@ export default function FAQPage() {
 
         <div className="faq__item">
           <h3>How many The Date are there? </h3>
-          <p>Since <a href="https://en.wikipedia.org/wiki/Unix_time">Unix Epoch</a>, One The Date a day was generated. </p>
+          <p>Since <a href="https://en.wikipedia.org/wiki/Unix_time">Unix Epoch</a>, one The Date was generated each earth day. </p>
 
           <p>
           The earliest The Date generated was <a href={formatOpenSeaLink("Asset", 0)}> {tokenIdToDateString(0)}</a>  (Token #0) and 
@@ -69,7 +69,8 @@ export default function FAQPage() {
             For claimed past The Date, you can purchase it on the secondary market <a href={openseaLink}>OpenSea</a>.
           </p>
           <p>
-            For unclaimed past The Date, you could <a href={`${etherscanLinkOfToken}#writeContract`}>claim</a> it directly the contract. Select an available date to claim, and note that only one The Date will be available for each date. Each initial claim will cost Ξ 0.01
+            For unclaimed past The Date, you can <a href={`${etherscanLinkOfToken}#writeContract`}>claim</a> it directly in the contract. 
+            Select an available date to claim, and note that only one The Date will be available for each date. Each initial claim costs Ξ 0.01.
           </p>
           <p>
             As an metadata-based NFT project, to respect the first of its kind, <a href="https://lootproject.com/">Loot</a>, all the Loot holders are free to  <a href={`${etherscanLinkOfToken}#writeContract`}>claim</a> The Date. 
@@ -81,7 +82,7 @@ export default function FAQPage() {
           <h3>What can I do with The Date as an owner? </h3>
           <p>
             As the owner of The Date, you could optionally <a href={`${etherscanLinkOfToken}#writeContract`}>engrave</a> a note (100 ASCII characters including spaces) at 0.01 ether. 
-            However, you have to pay 0.1 ether to <a href={`${etherscanLinkOfToken}#writeContract`}>erase</a> the existing engraved note before a new one can be engraved. 
+            However, youhave to pay Ξ 0.1 <a href={`${etherscanLinkOfToken}#writeContract`}>erase</a> the existing engraved note before a new one can be engraved. 
           </p>
         </div>
 
@@ -100,14 +101,14 @@ export default function FAQPage() {
         <div className="faq__item">
           <h3>Why there is The Date DAO? </h3>
           <p>
-            Also, the holders of The Date NFT automatically became the members of The Date DAO 
-            Community can vote on Snapshot to decide where this project to go. 
+            The holders of The Date NFT will automatically became the members of The Date DAO.
+            The Date DAO  members can vote on Snapshot to decide where this project to go.
             For example, a proposal can be promoted to <a href=""> setReservedPrice()</a>, 
             <a href=""> setMinimumBps()</a>
             <a href=""> setReservedPrice()</a>
             <a href=""> setReservedPrice()</a>
             the min Percentage for auction, the note-engraving price, or the note-erasing price, 
-            even the length of the note. 
+            even the length of the note. More importantly, we invite the community to create with us.
           </p>
         </div>
       </div>
